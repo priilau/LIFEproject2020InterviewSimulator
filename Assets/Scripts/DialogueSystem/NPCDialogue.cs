ef2176a89d149d03fc3818ad46b2e2180fca1489
plaupa <priit.laupa@gmail.com>
2020-04-14 19:22:20 +0300

    - Added comfort slider - Added Player Text Bubble to present larger sentences(needs more development but works)

--- Assets/Scripts/DialogueSystem/NPCDialogue.cs
+++ Assets/Scripts/DialogueSystem/NPCDialogue.cs
@@ -14,10 +14,15 @@ public class NPCDialogue : MonoBehaviour
     private GameObject npcText;
     private GameObject npcThoughtsBubble;
     private GameObject npcThoughtsText;
+    private GameObject playerTextBubble;
+    private GameObject playerText;
     private GameObject option1;
     private GameObject option2;
     private GameObject option3;
     private GameObject option4;
+    private GameObject comfortSlider;
+    private IEnumerator displayTextCoroutine;
+    private bool isCoroutineRunning = false;
 
     private int selectedOption = -2;  // exit node is -1
     
@@ -32,10 +37,13 @@ public class NPCDialogue : MonoBehaviour
         npcText = GameObject.Find("SpeechBubbleText");
         npcThoughtsBubble = GameObject.Find("ThoughtBubble");
         npcThoughtsText = GameObject.Find("ThoughtBubbleText");
+        playerTextBubble = GameObject.Find("PlayerTextBubble");
+        playerText = GameObject.Find("PlayerTextBubbleText");
         option1 = GameObject.Find("Option1");
         option2 = GameObject.Find("Option2");
         option3 = GameObject.Find("Option3");
         option4 = GameObject.Find("Option4");
+        comfortSlider = GameObject.Find("ComfortSlider");
 
         RunDialogue();
     }
@@ -50,13 +58,41 @@ public class NPCDialogue : MonoBehaviour
         selectedOption = x;
     }
 
+    private IEnumerator DisplayText(string text, GameObject textObject)
+    {
+        isCoroutineRunning = true;
+        char[] textChars;
+        string newText;
+        
+        textChars = text.ToCharArray(0, text.Length);
+        for (int i = 0; i < textChars.Length; i++)
+        {
+            newText = npcText.GetComponent<Text>().text;
+            textObject.GetComponent<Text>().text = newText + textChars[i];
+            yield return new WaitForSeconds(0.03f);
+        }
+        isCoroutineRunning = false;
+    }
+
     private void DisplayNode(DialogueNode node)
     {
+        playerTextBubble.SetActive(false);
+        if (node.PlayerText.Length > 0)
+        {
+            playerText.GetComponent<Text>().text = node.PlayerText;
+            playerTextBubble.SetActive(true);
+            // TODO: make the text write one letter at a time correctly. This needs to finish before the options become visible.
+            //displayTextCoroutine = DisplayText(node.PlayerText, playerText);
+            //StartCoroutine(displayTextCoroutine);
+        }
+
         npcSpeechBubble.SetActive(false);
         if(node.Text.Length > 0)
         {
-            npcText.GetComponent<Text>().text = node.Text;
+            npcText.GetComponent<Text>().text = "";
             npcSpeechBubble.SetActive(true);
+            displayTextCoroutine = DisplayText(node.Text, npcText);
+            StartCoroutine(displayTextCoroutine);
         }
 
         npcThoughtsBubble.SetActive(false);
@@ -64,6 +100,11 @@ public class NPCDialogue : MonoBehaviour
         {
             npcThoughtsText.GetComponent<Text>().text = node.Thoughts;
             npcThoughtsBubble.SetActive(true);
+        }
+
+        if(node.ScaleValue != 0)
+        {
+            comfortSlider.GetComponent<ComfortSlider>().SetComfortValue(node.ScaleValue);
         }
 
         option1.SetActive(false);
@@ -98,6 +139,14 @@ public class NPCDialogue : MonoBehaviour
         btn.GetComponent<Button>().onClick.AddListener(delegate
         {
             SetSelectedOption(opt.DestNodeID);
+            if (isCoroutineRunning)
+            {
+                StopCoroutine(displayTextCoroutine);
+            }
+            if(opt.DestNodeID == -1) 
+            {
+                // TODO: go to feedback screen
+            }
         });
     }
 
---
+++ Assets/Scripts/NPC/ComfortSlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComfortSlider : MonoBehaviour
+{
+    private GameObject comfortSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        comfortSlider = GameObject.Find("ComfortSlider");
+    }
+    public void SetComfortValue(int value)
+    {
+        comfortSlider.GetComponent<Slider>().value += value;
+    }
+}
