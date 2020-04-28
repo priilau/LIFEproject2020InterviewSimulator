db1a032e33df18f77debf3071b543bf873a3b48a
petsens <peeterr@tlu.ee>
2020-04-28 19:02:36 +0300

    Started making table and components on interview scene

---
+++ Assets/Scripts/UI/Interview/InterviewBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.UI.ItemSelection;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InterviewBehaviour : MonoBehaviour
+{
+
+    public TextAsset jsonFile;
+    public Items itemList;
+    
+    void Start()
+    {
+
+        itemList = JsonUtility.FromJson<Items>(jsonFile.text);
+
+        foreach (Item item in itemList.items)
+        {
+            GameObject itemGameObject = GameObject.Find(item.gameObjectName);
+            if (!PlayerData.selectedItems.Contains(item))
+            {
+                itemGameObject.SetActive(false);
+                Debug.Log("vänt");
+            }
+        }
+
+    }
+}
