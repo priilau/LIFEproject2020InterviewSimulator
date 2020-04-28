37e7aa603d1d06db2cd5e42b29b4396fe7a9eaf6
plaupa <priit.laupa@gmail.com>
2020-04-28 20:47:48 +0300

    - Debuging InterviewBehavior for items on table

--- Assets/Scripts/UI/Interview/InterviewBehaviour.cs
+++ Assets/Scripts/UI/Interview/InterviewBehaviour.cs
@@ -1,30 +1,25 @@
 ﻿using Assets.Scripts.UI.ItemSelection;
-using System.Collections;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class InterviewBehaviour : MonoBehaviour
 {
-
     public TextAsset jsonFile;
     public Items itemList;
-    
     void Start()
     {
-
         itemList = JsonUtility.FromJson<Items>(jsonFile.text);
 
         foreach (Item item in itemList.items)
         {
             GameObject itemGameObject = GameObject.Find(item.gameObjectName);
-            if (!PlayerData.selectedItems.Contains(item))
+            if (itemGameObject)
             {
-                itemGameObject.SetActive(false);
-                Debug.Log("vänt");
+                Debug.Log(itemGameObject);
+                if (!PlayerData.selectedItems.Contains(item))
+                {
+                    itemGameObject.SetActive(false);
+                }
             }
         }
-
     }
 }
